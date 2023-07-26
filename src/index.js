0bbc7d7a394d854a7aa55af70431d1a526bd80b1
Rafael Gomes Heuseler <rafaelheuseler@hotmail.com>
2023-07-26 18:20:53 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/usuarios', (req, res) =>{
+    const usuarios = [
+    {id: 11, nome: 'joao', idade: 23},
+    {id: 2, nome: 'maria', idade: 18},
+    {id: 4, nome: 'ana', idade: 30},
+    {id: 1, nome: 'rodrigo', idade: 17},
+    {id: 123, nome: 'rodrigo', idade: 17}
+    
+    ]
+
+    
+    res.send(usuarios)
+})
+
+app.listen(3000);
